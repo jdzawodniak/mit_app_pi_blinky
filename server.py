b34e4c290963557c037220e0bc650480f5714d5d
jdzawodniak <38980694+jdzawodniak@users.noreply.github.com>
2019-05-31 13:26:29 -0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,23 @@
+################################
+##Generated with a lot of love##
+##    with   EasyPython       ##
+##Web site: easycoding.tn     ##
+################################
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler_httpd(BaseHTTPRequestHandler):
+  def do_GET(self):
+    messagetosend = bytes('HELLO WORLD',"utf")
+    self.send_response(200)
+    self.send_header('Content-Type', 'text/plain')
+    self.send_header('Content-Length', len(messagetosend))
+    self.end_headers()
+    self.wfile.write(messagetosend)
+    print(self.requestline)
+    return
+
+
+server_address_httpd = ('192.168.86.54',8080)
+httpd = HTTPServer(server_address_httpd, RequestHandler_httpd)
+print('Starting Server:')
+httpd.serve_forever()
